45109a7649fa9692af41603285a7967ab0794a9e
root <root@ubuntu.ubuntu-domain>
2013-11-21 02:26:00 -0300

    IA burra, falta fazer movimentos randômicos

--- src/matrixcontroller.c
+++ src/matrixcontroller.c
@@ -118,9 +118,9 @@ void initGameMatrix()
 /*
     Checks if happened a collision
 */
-int isStepOk()
+int isStepOk(Player thisPlayer)
 {
-    int matrixPosition = globalMatrix[mainPlayer.position.x][mainPlayer.position.y];
+    int matrixPosition = globalMatrix[thisPlayer.position.x][thisPlayer.position.y];
     int result;
     switch(matrixPosition)
     {
@@ -140,20 +140,111 @@ int isStepOk()
     return result;
 }
 
+void IAofEnemy(Player *enemy){
+    int enemyStep = isStepOk(*enemy);
+
+    if((enemyStep==STEP_COLLISION_TRAIL) || (enemyStep==STEP_COLLISION_WALL))
+    {
+        switch(enemy->direction)
+    {
+        case DIRECTION_LEFT:{
+                enemy->position.x = enemy->position.x + 1;
+                if(globalMatrix[enemy->position.x][enemy->position.y+1] == GAME_SPACE){
+                    enemy->position.y = enemy->position.y + 1;
+                    enemy->direction = DIRECTION_DOWN;
+                }
+                else
+                    if(globalMatrix[enemy->position.x][enemy->position.y-1] == GAME_SPACE){
+                         enemy->position.y = enemy->position.y - 1;
+                         enemy->direction = DIRECTION_UP;
+                    }
+            }
+        break;
+        case DIRECTION_UP:{
+                enemy->position.y = enemy->position.y + 1;
+                if(globalMatrix[enemy->position.x+1][enemy->position.y] == GAME_SPACE){
+                    enemy->position.x = enemy->position.x + 1;
+                    enemy->direction = DIRECTION_RIGHT;
+                }
+                else
+                    if(globalMatrix[enemy->position.x-1][enemy->position.y] == GAME_SPACE){
+                         enemy->position.x = enemy->position.x - 1;
+                         enemy->direction = DIRECTION_LEFT;
+                    }
+            }
+        break;
+        case DIRECTION_RIGHT:{
+                enemy->position.x = enemy->position.x - 1;
+                if(globalMatrix[enemy->position.x][enemy->position.y+1] == GAME_SPACE){
+                    enemy->position.y = enemy->position.y + 1;
+                    enemy->direction = DIRECTION_DOWN;
+                }
+                else
+                    if(globalMatrix[enemy->position.x][enemy->position.y-1] == GAME_SPACE){
+                         enemy->position.y = enemy->position.y - 1;
+                         enemy->direction = DIRECTION_UP;
+                    }
+            }
+        break;
+        case DIRECTION_DOWN:
+        default:{
+                enemy->position.y = enemy->position.y - 1;
+                 if(globalMatrix[enemy->position.x+1][enemy->position.y] == GAME_SPACE){
+                    enemy->position.x = enemy->position.x + 1;
+                    enemy->direction = DIRECTION_RIGHT;
+                }
+                else
+                    if(globalMatrix[enemy->position.x-1][enemy->position.y] == GAME_SPACE){
+                         enemy->position.x = enemy->position.x - 1;
+                         enemy->direction = DIRECTION_LEFT;
+                    }
+        break;
+        }
+    }
+    }
+}
+
+/*
+void translateMove(Player enemy)
+{
+    switch(enemy.direction)
+    {
+        case DIRECTION_LEFT:
+        {
+
+        }
+        case DIRECTION_UP:
+        {
+
+        }
+        case DIRECTION_RIGHT:
+        {
+
+        }
+        case DIRECTION_DOWN:
+        {
+
+        }
+    }
+}
+*/
+
 int gameStep()
 {
 
-    int step;
+    int step, step_aux;
 
     globalMatrix[mainPlayer.position.x][mainPlayer.position.y] = GAME_TRAIL;
 	globalMatrix[enemy1.position.x][enemy1.position.y] = GAME_TRAIL;
 	globalMatrix[enemy2.position.x][enemy2.position.y] = GAME_TRAIL;
 	
     incrementElementPosition( &(mainPlayer.position), mainPlayer.direction );
-	incrementElementPosition( &(enemy1.position), enemy1.direction );
+    incrementElementPosition( &(enemy1.position), enemy1.direction );
 	incrementElementPosition( &(enemy2.position), enemy2.direction );
 
-    step = isStepOk();
+    step = isStepOk(mainPlayer);
+    IAofEnemy(&enemy1);
+    IAofEnemy(&enemy2);
 
     globalMatrix[mainPlayer.position.x][mainPlayer.position.y] = GAME_PLAYER;
 	globalMatrix[enemy1.position.x][enemy1.position.y] = GAME_ENEMY;
